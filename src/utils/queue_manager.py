3a9821827219f17e16a9be9274da246657440d37
yanyao <zdc666nb@gmail.com>
2023-09-30 22:24:39 +0800

    简单修改一下队列管理器

--- src/utils/queue_manager.py
+++ src/utils/queue_manager.py
@@ -9,12 +9,9 @@ class QueueManager:
     """队列管理器"""
 
     def __init__(self):
-        self.queues = {
-            "summarize": asyncio.Queue(),
-            "evaluate": asyncio.Queue(),
-            "reply": asyncio.Queue(),
-            "private": asyncio.Queue(),
-        }
+        self.queues = {}
 
     def get_queue(self, queue_name: str):
+        if queue_name not in self.queues:
+            self.queues[queue_name] = asyncio.Queue()
         return self.queues.get(queue_name)
