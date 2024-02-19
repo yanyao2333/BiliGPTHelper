cac5623f59593cbcd0bdc495f649220a42326259
yanyao <zdc666nb@gmail.com>
2024-02-20 00:10:12 +0800

    fix(config): 更改配置文件模板

--- src/core/app.py
+++ src/core/app.py
@@ -45,7 +45,7 @@ class BiliGPT(Module):
             # }
             # yaml.dump(Config().model_dump(mode="python"))
             _LOGGER.error(
-                f"配置文件格式错误 可能是因为项目更新、配置文件添加了新字段，请自行检查配置文件格式并更新配置文件 已复制最新配置文件模板到 config_template.yml 下面将打印详细错误日志"
+                "配置文件格式错误 可能是因为项目更新、配置文件添加了新字段，请自行检查配置文件格式并更新配置文件 已复制最新配置文件模板到 config_template.yml 下面将打印详细错误日志"
             )
             raise ConfigError(f"配置文件格式错误：{e}") from e
         return config
